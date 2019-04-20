cd56a51b9e31967520107cbd0963da8377732c16
Makan Fofana <mfofanaT99@gmail.com>
2019-04-20 14:07:28 -0400

    Initial Commit

---
+++ KivaLoan/Model/Loan.swift
@@ -0,0 +1,18 @@
+//
+//  Loan.swift
+//  KivaLoan
+//
+//  Created by Makan fofana on 4/20/19.
+//  Copyright © 2019 AppCoda. All rights reserved.
+//
+
+import Foundation
+
+struct Loan {
+    
+    var name: String = ""
+    var country: String = ""
+    var use: String = ""
+    var amount: Int = 0
+    
+}
