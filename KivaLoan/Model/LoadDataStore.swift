ef2e0904eb7a14de0a9a5fa2a2f70e0ba5f91395
Makan Fofana <mfofanaT99@gmail.com>
2019-04-23 20:00:10 -0400

    Creation of LoanDataStore struct to take in loans array using codeable.

---
+++ KivaLoan/Model/LoadDataStore.swift
@@ -0,0 +1,14 @@
+//
+//  LoadDataStore.swift
+//  KivaLoan
+//
+//  Created by Makan fofana on 4/23/19.
+//  Copyright © 2019 Makan Fofana. All rights reserved.
+//
+
+import Foundation
+
+
+struct LoanDataStore: Codable {
+    var loans: [Loan]
+}
